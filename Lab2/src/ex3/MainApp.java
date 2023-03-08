6db3650cfb2d5a26934e07dcaed0296db4f450aa
ursu <dumitru.ursu@student.upt.ro>
2023-03-08 19:53:22 +0200

    ex4

--- Lab2/src/ex3/MainApp.java
+++ Lab2/src/ex3/MainApp.java
@@ -1,5 +1,37 @@
 package ex3;
 
-public class MainApp { //substring(a, b), String Builder
+import java.util.Scanner;
 
+public class MainApp {
+    public static void main(String[] args) {
+        String str;
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Introdu un string: ");
+        str = scan.nextLine();
+
+        StringBuilder strB = new StringBuilder(str);
+
+        System.out.print("Introdu un alt string: ");
+        str = scan.nextLine();
+        int n;
+
+        do {
+            System.out.print("Introdu pozitia din care inserezi: ");
+            n = scan.nextInt();
+        } while(n >= str.length() || n < 0);
+
+        System.out.println("Stringul nou : " + strB.insert(n, str));
+
+        int m;
+        do {
+            System.out.print("Introdu pozitia din care incepi stergerea: ");
+            n = scan.nextInt();
+            System.out.print("Introdu pozitia in care se termina stergerea: ");
+            m = scan.nextInt();
+        } while((n >= str.length() || n < 0)
+                && (m >= str.length() || m < 0));
+
+        System.out.println("Stringul rezultat : " + strB.delete(n, m));
+    }
 }
